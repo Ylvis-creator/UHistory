97916dd2ceef3fb0c55aba0342a7f530248a5eae
Ylvis-creator <bours.martin@icloud.com>
2025-12-12 16:41:24 +0100

    Update game.js

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -1,27 +1,11 @@
-Joueur : <b>${playerName}</b><br>
-	Code de partie : <b>${gameCode}</b><br>
-	Année de départ : ${year}<br><br>
-	Le monde est prêt.Appuie sur 'SUIVANT' pour avancer l'histoire !
-	';
-}
+ocument.getElementById("startBtn").addEventListener("click", () => {
+    const name = document.getElementById("playerName").value.trim();
 
-function generateGameCode() {
-   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   let code = "UH-";
+    if (name === "") {
+        alert("Entre un nom avant de commencer !");
+        return;
+    }
 
-for (let i = 0; i < 4; i++) {
-   code += chars[Math.floor(Math.random() * chars.length)];
-   }
-return code;
-}
-
-
-function initializeCountries() {
-   countries = [
-      { name: "France" , economy: 110, army: 90, stability: 100 },
-	  { name: "USA", economy: 130, army: 130, stability: 60 },
-	  { name: "Russie", economy: 80, army: 110, stability: 100 },
-	  { name: "Chine", economy: 120, army: 100, stability: 80 },
-	  { name: "Inde", economy: 90, army: 80, stability: 130 },
-	  { name: "Royaume-Uni", economy: 100, army: 80, stability: 120 },
-	  
+    document.getElementById("output").innerText =
+        "Bienvenue " + name + " ! Votre partie commence en 2000...";
+});
